68782e823fb5038a6a3220535df586ac8139801c
DevBoxFanBoy <chris.laule90@gmail.com>
2021-01-23 05:40:30 +0100

    Fix host for docker containers
    Now we can request calls against the docker container.

--- pkg/config/configuration.go
+++ pkg/config/configuration.go
@@ -3,6 +3,6 @@ package config
 type Config struct {
 	Server struct {
 		Port string `yaml:"port" env:"SERVER_PORT" env-default:"8080"`
-		Host string `yaml:"host" env:"SERVER_HOST" env-default:"localhost"`
+		Host string `yaml:"host" env:"SERVER_HOST" env-default:"0.0.0.0"`
 	} `yaml:"server"`
 }
